bda68f4d14d1cdfd06699961b33298589e7729d3
Candice Lee <candice.lee101@gmail.com>
2020-05-11 16:33:24 +0800

    revised version2

--- password.py
+++ password.py
@@ -2,10 +2,12 @@ password  = 'a123456'
 i = 3 #剩餘機會
 
 while i > 0:
+	i = i - 1
 	if input('請輸入密碼: ') == password:
 		print('登入成功')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤! 還有', i, '次機會')
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有', i, '次機會')
 		
